4cdc5f983f14e81f10c10a64bdb7dec5ac18a7e1
AkaGitash <2000520200011@ietlucknow.ac.in>
2024-02-08 20:27:07 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import rcgjsx from './components/randomcolorgenerator';
+import RandomColor from './components/randomcolorgenerator';
+
+function App() {
+  return (
+    <div className="App">
+      {/* {Accordian Component} */}
+      {/* <Accordian/> */}
+      {/* Random Color Component */}
+      <RandomColor/>
+    </div>
+  );
+}
+
+export default App;
